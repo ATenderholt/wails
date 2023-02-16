0192182d4bb3ba834943b62a7c2c70c413f397e3
Adam Tenderholt <atenderholt@gmail.com>
2023-02-16 05:58:27 -0800

    Parser: more simple AstCallExpr.String()

--- v3/internal/parser/ast_callexpr.go
+++ v3/internal/parser/ast_callexpr.go
@@ -1,8 +1,8 @@
 package parser
 
 import (
+	"fmt"
 	"go/ast"
-	"strings"
 )
 
 // AstCallExpr is type definition of ast.CallExpr with functions to make it easier to use
@@ -10,31 +10,13 @@ type AstCallExpr ast.CallExpr
 
 // String returns the string representation of an ast.CallExpr function name
 func (expr *AstCallExpr) String() string {
-	// check if non-packaged (i.e. not a selector) function
-	ident, ok := expr.Fun.(*ast.Ident)
-	if ok {
-		return ident.Name
+	switch t := expr.Fun.(type) {
+	case *ast.Ident:
+		return t.Name
+	case *ast.SelectorExpr:
+		return AstSelectorExpr(*t).String()
+	default:
+		msg := fmt.Sprintf("[AstCallExpr.String()] unsupported type for F: %T", t)
+		panic(msg)
 	}
-
-	selectorExpr, ok := expr.Fun.(*ast.SelectorExpr)
-	if !ok {
-		panic("AstCallExpr has Fun that cannot be cast to *ast.SelectorExpr")
-	}
-
-	if selectorExpr.Sel == nil {
-		panic("AstCallExpr has nil Sel")
-	}
-
-	var builder strings.Builder
-	if selectorExpr.X != nil {
-		x, ok := selectorExpr.X.(*ast.Ident)
-		if !ok {
-			panic("AstCallExpr has X that cannot be cast to *ast.Ident")
-		}
-		builder.WriteString(x.Name)
-		builder.WriteRune('.')
-	}
-
-	builder.WriteString(selectorExpr.Sel.Name)
-	return builder.String()
 }
